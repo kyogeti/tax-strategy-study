6c20ee6d9cd816253713f0ce377df3454c789d47
William Amorim <william.amorim@stone.com.br>
2022-12-31 21:18:07 -0300

    Update coverage

---
+++ tests/TaxStrategy.UnitTests/Models/ProductTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using TaxStrategy.Models;
+
+namespace TaxStrategy.UnitTests.Models;
+
+public class ProductTests
+{
+    private readonly IFixture _fixture;
+
+    public ProductTests()
+    {
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public void Product_GivenConstructor_ShouldBuildAsExpected()
+    {
+        var expectedName = "xpto";
+        var expectedCode = "xptocode";
+        var expectedPrice = 2.99m;
+
+        var product = new Product(expectedName, expectedCode, expectedPrice, 
+            _fixture.CreateMany<ApplicableTax>(3));
+
+        product.Should().Match<Product>(src => 
+            src.Name == expectedName &&
+            src.Code == expectedCode &&
+            src.Price == expectedPrice);
+    }
+}
